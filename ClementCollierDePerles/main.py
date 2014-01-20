594902ad9d76ba8deeb0a82d6cf9213de34030cb
GabrielG-S <gabriel.goutte-solard@insa-lyon.fr>
2014-01-20 17:38:04 +0100

    Ajout des images

--- ClementCollierDePerles/main.py
+++ ClementCollierDePerles/main.py
@@ -1,4 +1,4 @@
-# -*-coding: utf-8 -*-  # TEST POUR GABRIEL
+# -*-coding: utf-8 -*-
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.colorpicker import *
@@ -6,45 +6,42 @@ from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProper
 from random import randint
 from kivy.core.audio import SoundLoader
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 
 class Perle(Widget):
-        couleur = NumericProperty(0)                      
+    couleur = NumericProperty(0)                      
         
 class JeuPerles(Widget):        
-        perleVoy0 = ObjectProperty(None) #rouge
-        perleVoyU = ObjectProperty(None) #bleu
+    perleVoy0 = ObjectProperty(None) #rouge
+    perleVoyU = ObjectProperty(None) #bleu
         
-        voyDispo = ['O', 'U']
+    voyDispo = ['O', 'U'] #liste des voyelles disponibles
+    a=0
+    print voyDispo
+    print len(voyDispo)
 
-        def consigneAlea():
-			global voyDispo
-			a = randint(0, voyDispo.len-1)
-			print(a)
-			if(a==0):
-				return "rouge.wav"
-			elif(a==1):
-				return "bleu.wav"
-        
-        
-        #consigneAlea()
-        
-        
-        
-        sound = SoundLoader.load('bleu.wav')
-        if sound:
-            print("Sound found at %s" % sound.source)
+    def consigneAlea(voyDispo):   #J'ai passé voyDispo en variable locale puisque ce n'est pas dans cette fonction qu'on la modifiera
+        global a
+        a = randint( 0, len(voyDispo)-1 )
+        print(a)
+        if(a==0):
+            return "rouge.wav"
+        elif(a==1):
+            return "bleu.wav"    
+               
+    sound = SoundLoader.load( consigneAlea(voyDispo) )
+    if sound:
+        print("Sound found at %s" % sound.source)
             
-            sound.play()
+        sound.play()
             
         
         
-        def on_touch_move(self, touch):
-                if self.perleVoyO.collide_point(touch.x,touch.y):
-                        self.perleVoyO.center = touch.pos
-                elif self.perleVoyU.collide_point(touch.x,touch.y):
-                        self.perleVoyU.center=touch.pos
-                        
-                
+    def on_touch_move(self, touch):
+        if self.perleVoyO.collide_point(touch.x,touch.y):
+            self.perleVoyO.center = touch.pos
+        elif self.perleVoyU.collide_point(touch.x,touch.y):
+            self.perleVoyU.center=touch.pos
         
         print()
         
